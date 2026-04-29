ee54afc8a3bcf71d099652f04575faff7bbabbe7
Sophia Mogere <mogeresophia@gmail.com>
2026-04-29 23:17:43 +0300

    Update script

--- imatinib_mouse_cAbl_kinase/scripts/imatinib_docking.sh
+++ imatinib_mouse_cAbl_kinase/scripts/imatinib_docking.sh
@@ -1,40 +1,34 @@
 #!usr/bin/env bash
 
 ##Installation
-#conda create -n vina python=3
-#conda activate vina
-#conda config --env --add channels conda-forge
-#conda install -c conda-forge numpy swig boost-cpp libboost sphinx sphinx_rtd_theme
-#pip install vina
-#pip install -U scipy rdkit meeko gemmi prody
-#conda install -c conda-forge autogrid
+conda create -n vina python=3
+conda activate vina
+conda config --env --add channels conda-forge
+conda install -c conda-forge numpy swig boost-cpp libboost sphinx sphinx_rtd_theme
+pip install vina
+pip install -U scipy rdkit meeko gemmi prody
+conda install -c conda-forge autogrid
 
 ###Turns out vina wasn't installed and I ran into conflicts due to Python 3.14 :(
 #Create a separate conda environment for vina
-#conda create -n vina_dock python=3.11
-#conda activate vina_dock
-#conda config --add channels conda-forge
-#conda config --add channels bioconda
-#conda config --set channel_priority strict
-#conda install vina
+conda create -n vina_dock python=3.11
+conda activate vina_dock
+conda config --add channels conda-forge
+conda config --add channels bioconda
+conda config --set channel_priority strict
+conda install vina
 
 #Prepare the receptor for docking: remember to remove the ligands prior to running this
-#conda activate vina
-
-#mk_prepare_receptor.py -i data/1IEP_clean_receptor.pdb -o output/1IEP_prep_receptor -p -v \
-#  --box_size 20 20 20 --box_center 15.190 53.903 16.917
+mk_prepare_receptor.py -i data/1IEP_clean_receptor.pdb -o output/1IEP_prep_receptor -p -v \
+  --box_size 20 20 20 --box_center 15.190 53.903 16.917
 
 #View the coordinates
-#cat 1IEP_prep_receptor.box.txt
+cat 1IEP_prep_receptor.box.txt
 
 #Prepare the ligand for docking. Download the SDF format of imatinib from Pubchem: https://pubchem.ncbi.nlm.nih.gov/compound/5291
-#mk_prepare_ligand.py -i data/imatinib_ligand_COMPOUND_CID_5291.sdf -o output/imatinib_ligand.pdbqt
+mk_prepare_ligand.py -i data/imatinib_ligand_COMPOUND_CID_5291.sdf -o output/imatinib_ligand.pdbqt
 
 #Docking
-#conda deactivate
-
-#conda activate vina_dock
-
 vina --receptor output/1IEP_prep_receptor.pdbqt --ligand output/imatinib_ligand.pdbqt \
   --config output/1IEP_prep_receptor.box.txt \
   --exhaustiveness=32 \
